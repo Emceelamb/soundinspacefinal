6265b05c8adcff1f8a288b9d56c240669d3d6e1a
emceelamb <mlam@berkeley.edu>
2020-04-20 12:10:11 -0400

    change routes

--- server.js
+++ server.js
@@ -93,7 +93,7 @@ app.get('/', (req, res)=>{
 });
 
 app.get('/synth', (req, res)=>{
-  res.sendFile(path.join(__dirname +'/sytnh.html'));
+  res.sendFile(path.join(__dirname +'/public/synth.html'));
 });
 
 server.listen(PORT, ()=>{
